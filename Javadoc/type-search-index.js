1971e115229ac3a9857b8293b0e1f0cb3b46d36e
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2023-12-23 20:07:56 -0500

    Generated javadocs

---
+++ Javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"StrVis","l":"DataExtractor"},{"p":"StrVis","l":"Main"},{"p":"StrVis","l":"MainWindow"},{"p":"StrVis","l":"Visualizations"}];updateSearchResults();
