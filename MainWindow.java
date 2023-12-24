82ca84aebef7cf51cbc169259c97859c6c9ef7d7
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2023-12-23 19:28:38 -0500

    Created mainframe and button that gets abs path of a file

---
+++ MainWindow.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+
+public class MainWindow implements ActionListener{
+
+    private JFrame mainFrame;
+    private JButton fileUpButton;
+
+    MainWindow(){
+        this.mainFrame = new JFrame("Strong Data Visualizer");
+        this.fileUpButton = new JButton("Upload File");
+    }
+
+    public void createWindow(){
+
+        // Create and set up the window
+        this.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+
+        initFileUpButton();
+
+        // Display the window
+        this.mainFrame.setLocationRelativeTo(null);
+        this.mainFrame.pack(); // Pack sets the screensize to be big enough for all the components in it
+        this.mainFrame.setVisible(true);
+        
+
+    }
+
+    private void initFileUpButton(){
+        JPanel buttonPanel = new JPanel();
+        JPanel containerPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(1,1));
+
+        // Turn off the border around the button text
+        this.fileUpButton.setFocusable(false);
+
+        this.fileUpButton.addActionListener(this);
+
+        buttonPanel.add(this.fileUpButton);
+        containerPanel.add(buttonPanel);
+        this.mainFrame.getContentPane().add(containerPanel);
+
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent evt){
+        if(evt.getSource() == this.fileUpButton){
+            JFileChooser file_upload = new JFileChooser();
+            //file_upload.showOpenDialog(null);
+
+            int response = file_upload.showOpenDialog(null);
+
+            if(response  == JFileChooser.APPROVE_OPTION){
+                File file_path = new File(file_upload.getSelectedFile().getAbsolutePath());
+                System.out.println(file_path);
+
+            }
+        }
+
+    }
+
+    
+
+
+}
+
