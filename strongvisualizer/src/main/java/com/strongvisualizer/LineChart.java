efaf032af8eb48c716a028152e849a2b33ec287f
Thomas Boyle <90807000+20tsb3@users.noreply.github.com>
2024-01-05 16:45:51 -0500

    Commented out main function

--- strongvisualizer/src/main/java/com/strongvisualizer/LineChart.java
+++ strongvisualizer/src/main/java/com/strongvisualizer/LineChart.java
@@ -51,7 +51,7 @@ public class LineChart extends ApplicationFrame{
         return dataset;
     }
 
-    public static void main( String[ ] args ) {
+    //public static void main( String[ ] args ) {
         // LineChart chart = new LineChart(
         //    "School Vs Years" ,
         //    "Numer of Schools vs years", 
@@ -60,7 +60,7 @@ public class LineChart extends ApplicationFrame{
   
         // chart.pack();
         // chart.setVisible(true);
-     }
+     //}
 
 
 }
